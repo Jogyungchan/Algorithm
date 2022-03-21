2541239f84ba8ac3ee9018ea37ab7c0450cdb1da
Jogyungchan <91017507+Jogyungchan@users.noreply.github.com>
2022-03-22 00:53:03 +0900

    while문 1번(A+B -5)

---
+++ Baekjoon/while문/B_10952.java
@@ -0,0 +1,23 @@
+package _while;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_10952 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if(a+b==0) break;
+			sb.append(a+b).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
